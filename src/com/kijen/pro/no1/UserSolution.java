632728a8abb59c616df289a58ac1c28eb7b16061
kijen723 <kij2646@gmail.com>
2024-03-11 17:40:57 +0900

    fix pro/no1: Uncomplete

--- src/com/kijen/pro/no1/UserSolution.java
+++ src/com/kijen/pro/no1/UserSolution.java
@@ -1,24 +1,77 @@
 package com.kijen.pro.no1;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 class UserSolution {
-    void init(int N, int mSoldier[][], char mMonarch[][][])
-    {
+    Monarch[][] map;
+    HashMap<String, String> allies;
+    HashMap<String, ArrayList<String>> enemies;
+
+    class Monarch {
+        int cnt;
+        String name;
+
+        public Monarch(int cnt, String name) {
+            this.cnt = cnt;
+            this.name = name;
+        }
+    }
+
+    void init(int N, int mSoldier[][], char mMonarch[][][]) {
+        map = new Monarch[N][N];
+        allies = new HashMap<>();
+        enemies = new HashMap<>();
+
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
+                map[r][c] = new Monarch(mSoldier[r][c], new String(mMonarch[r][c]));
+                allies.put(map[r][c].name, map[r][c].name);
+            }
+        }
+    }
+
+    void destroy() {
 
     }
-    void destroy()
-    {
 
-    }
-    int ally(char mMonarchA[], char mMonarchB[])
-    {
+    int ally(char mMonarchA[], char mMonarchB[]) {
+        String a = new String(mMonarchA);
+        String b = new String(mMonarchB);
+
+        unionSet(a, b);
+
+
         return -4;
     }
-    int attack(char mMonarchA[], char mMonarchB[], char mGeneral[])
-    {
+
+    int attack(char mMonarchA[], char mMonarchB[], char mGeneral[]) {
         return -3;
     }
-    int recruit(char mMonarch[], int mNum, int mOption)
-    {
+
+    int recruit(char mMonarch[], int mNum, int mOption) {
         return -1;
+    }
+
+    /* My function */
+    void unionSet(String mMonarchA, String mMonarchB) {
+        String a = findSet(mMonarchA);
+        String b = findSet(mMonarchB);
+
+        if (a.compareTo(b) < 0) {
+            allies.put(a, b);
+        } else {
+            allies.put(b, a);
+        }
+
+
+    }
+
+    String findSet(String mMonarch) {
+        if (allies.get(mMonarch).equals(mMonarch)) {
+            return mMonarch;
+        } else {
+            return allies.put(mMonarch, findSet(mMonarch));
+        }
     }
 }
